6c5ae510461bd626247ed22a75beda2a0c6db428
hisjeans <his7jeans@gmail.com>
2026-03-17 14:32:19 +0900

    콘솔로그와 문자열 보간법 학습

---
+++ swift-basic-study/Person.swift
@@ -0,0 +1,7 @@
+//
+//  Person.swift
+//  swift-basic-study
+//
+//  Created by 최진희 on 3/17/26.
+//
+
--- swift-basic-study/main.swift
+++ swift-basic-study/main.swift
@@ -1,11 +1,31 @@
-//
-//  main.swift
-//  swift-basic-study
-//
-//  Created by 최진희 on 3/17/26.
-//
+import Swift
 
-import Foundation
+let age: Int=10
 
-print("Hello, World!")
+//문자열에서 어떤 값 치환해 넣고 싶다면 문자열 보간법 사용
+"안녕하세요! 저는 \(age)살입니다"
+// =="안녕하세요! 저는 10살입니다"
+// "\()" 사이 "age" 상수값, 실질적인 값 치환
 
+"안녕하세요! 저는 \(age+5)살입니다"
+// =="안녕하세요! 저는 15살입니다" - 수식 계산 가능
+
+print("안녕하세요! 저는 \(age+5)살입니다")
+
+print("\n####################\n")
+
+//콘솔에서 실질적으로 보고 싶다면 "print" or "dump"
+
+class Person{
+    var name: String="yushi"
+    var age: Int=10
+}
+
+let yushi: Person=Person()
+
+print(yushi)
+
+print("\n####################\n")
+
+dump(yushi)
+//dump >>자세한 값 볼 수 있음>> print
